23806d55ed609084cf1651b54e705521075bd9bc
Faniel Negasi <ugr18809916@office.mu.edu.et>
2026-04-24 23:43:45 +0300

    Add background size cover to changeBg function

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function changeBg(bg, title){
     const banner =  document.querySelector('.banner');
     const contents =  document.querySelectorAll('.content');
     banner.style.background = 'url("../movies/$(bg)")';
+    banner.style.backgroundSize = 'cover';
     banner.style.backgroundPosition = 'center';
 
     contents.forEach(content => {
